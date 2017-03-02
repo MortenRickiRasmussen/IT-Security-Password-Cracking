e6c46ee7a2888dff9406d7275121b65abb6baf6d
MortyHD <mortyhd@live.dk>
2017-03-02 13:40:47 +0100

    ServerSetup2

--- src/dk/easj/server/Server.java
+++ src/dk/easj/server/Server.java
@@ -1,5 +1,7 @@
 package dk.easj.server;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -18,9 +20,19 @@ public class Server implements Runnable {
     private ServerSocket serverSocket;
     private ArrayList<Slave> slaves;
     private boolean running;
+    private ArrayList<UserInfo> passwordFile;
+    private ArrayList<String> dictionary;
 
     public Server() {
-        slaves = new ArrayList<>();
+        try {
+            slaves = new ArrayList<>();
+            dictionary = new ArrayList<>();
+            passwordFile = new ArrayList<>();
+            readPasswordFile();
+            readDictionary();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void run() {
@@ -57,7 +69,40 @@ public class Server implements Runnable {
         }
     }
 
-    public void something(){
+    public void readDictionary() throws IOException {
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader("webster-dictionary.txt");
+            BufferedReader dictionary = new BufferedReader(fileReader);
+            while (dictionary.readLine() != null) {
+                this.dictionary.add(dictionary.readLine());
+            }
+        } finally {
+            if (fileReader != null) {
+                fileReader.close();
+            }
+        }
+    }
 
+
+    public void readPasswordFile() throws IOException {
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader("passwords.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while (true) {
+                String line = bufferedReader.readLine();
+                if (line == null) {
+                    break;
+                }
+                String[] parts = line.split(":");
+                UserInfo userInfo = new UserInfo(parts[0], parts[1]);
+                this.passwordFile.add(userInfo);
+            }
+        } finally {
+            if (fileReader != null) {
+                fileReader.close();
+            }
+        }
     }
 }
